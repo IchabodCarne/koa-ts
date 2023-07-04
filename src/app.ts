32d08344eb03d7f36297e51950fc92b6d79ddf30
Ichabod-Carne <18829025242@163.com>
2023-07-04 14:02:55 +0800

    update package.json

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,31 @@ import http from 'http'
 const app = new Koa()
 
 const serve = new http.Server(app.callback())
+const address = serve.address()
+const bind = typeof address === 'string' ? `pipe ${address}` : `port ${address?.port}`
 
 app.use(async (ctx) => {
   ctx.body = 'Hello world'
 })
 
 serve.listen(9000)
+
+serve.on('error', (error: any) => {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      break
+    default:
+      throw error
+  }
+})
 serve.on('listening', () => {
-  const address = serve.address()
-  console.log('Listening on', address)
+  console.log(`Listening on ${bind}`)
 })
